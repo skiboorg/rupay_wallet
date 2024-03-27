7ef20a273bb0d36807108ac235243cc32fe1a9c4
xxx <dd@dd.dd>
2024-03-27 16:53:34 +0300

    init

---
+++ dist/pwa/js/279.d0e9d108.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis["webpackChunkrupay_wallet"]=globalThis["webpackChunkrupay_wallet"]||[]).push([[279],{18279:(a,e,l)=>{function r(a,e){return" get "}l.r(e),l.d(e,{default:()=>s});var t=l(12807);const u={},n=(0,t.A)(u,[["render",r]]),s=n}}]);
