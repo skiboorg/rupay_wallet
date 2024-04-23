2aab770862a7a1e6f8220fa67cda40ff9be82559
xxx <dd@dd.dd>
2024-04-23 17:52:36 +0300

    init

---
+++ src/pages/marketplace/easy_profit.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import Card from "components/block/Card.vue";
+</script>
+
+<template>
+  <q-page padding>
+    <Card title="Easy Profit">
+    <p class="text-center">В разработке</p>
+    </Card>
+  </q-page>
+</template>
+
+<style scoped lang="sass">
+
+</style>
---
+++ src/pages/marketplace/trading_pool.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Card from "components/block/Card.vue";
+import {useP2PStore} from "stores/p2p";
+const {p2pUser} = useP2PStore()
+
+</script>
+
+<template>
+  <q-page padding>
+    <Card title="Trading Pool">
+      <template v-if="p2pUser?.in_trading_pool">
+        in pool
+      </template>
+      <template v-else>
+        <p class="text-center">В разработке</p>
+      </template>
+
+
+    </Card>
+  </q-page>
+</template>
+
+<style scoped lang="sass">
+
+</style>
