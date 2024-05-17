1dede384ad38bd775e53a4e261924da55994cc2a
xxx <ddnnss@mail.ru>
2024-05-17 11:05:40 +0300

    init

--- dist/pwa/js/279.d0e9d108.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis["webpackChunkrupay_wallet"]=globalThis["webpackChunkrupay_wallet"]||[]).push([[279],{18279:(a,e,l)=>{function r(a,e){return" get "}l.r(e),l.d(e,{default:()=>s});var t=l(12807);const u={},n=(0,t.A)(u,[["render",r]]),s=n}}]);
